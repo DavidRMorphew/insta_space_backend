606eb2a58b5aab57231d8178441e4c29f179cf2f
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-20 02:23:51 -0400

    add auth create with strong and wrapped  params

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,20 @@
 class AuthController < ApplicationController
+    wrap_parameters :user, include: [:username, :email, :password]
+    skip_before_action :authorized, only: [:create]
+    
+    def create
+        @user = User.find_by(username: user_params[:username])
+        if @user && @user.authenticate(user_params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+        else
+            render json: { message: "Username or Password Invalid" }, status: :unauthorized
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
