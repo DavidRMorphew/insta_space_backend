9a46172660b685f3e664ac3a2608a5c341aea15a
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-19 21:15:42 -0400

    build basic users create with strong_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    wrap_parameters :user, include: [:username, :email, :password]
+
+    def create
+        @user = User.new(user_params)
+        binding.pry
+        if @user.save
+
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
 end
