beb3e934f10db5908f5c55a3c99be9ee01103562
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-20 00:23:53 -0400

    add auth controller and auth and profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       resources :likes
       resources :images
       resources :users
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
     end
   end
 end
