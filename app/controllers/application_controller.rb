2febe6f13e682be95f90bf8d29d2024c22919ded
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-20 00:39:09 -0400

    add encode and decode token with JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload, ENV["SESSION_SECRET"])
+    end
+
+    def decoded_token(token)
+        JWT.decode(token, ENV["SESSION_SECRET"])[0]
+    end
 end
