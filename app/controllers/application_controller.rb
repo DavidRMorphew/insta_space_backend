ab649d9a23b75ba0a0c2279372661562b18810b3
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-20 01:20:02 -0400

    build jw token authorization, current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,27 @@
 class ApplicationController < ActionController::API
     def encode_token(payload)
-        JWT.encode(payload, ENV["SESSION_SECRET"])
+        JWT.encode(payload, ENV["SESSION_SECRET"], ‘HS256’)
     end
 
-    def decoded_token(token)
-        JWT.decode(token, ENV["SESSION_SECRET"])[0]
+    def decoded_token
+        if request.headers['Authorization']
+            token = request.headers['Authorization'].split(' ')[1]
+            begin
+                JWT.decode(token, ENV["SESSION_SECRET"], true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+        end
+    end
+
+    def user_id
+        decoded_token[0]['user_id'] if decoded_token
+    end
+
+    def current_user
+        @user ||= User.find_by(id: user_id) if user_id
+    end
+
+    def logged_in?
+        !!current_user
     end
 end
