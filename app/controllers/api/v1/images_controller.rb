7385e2ef46ed4d6100070e73b7ef43e5834a7fe1
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-21 12:03:39 -0400

    require authorization before image index

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ImagesController < ApplicationController
-    skip_before_action :authorized, only: [:index]
+    # skip_before_action :authorized, only: [:index]
     def index
         images = Api.fetch_images
         render json: images
